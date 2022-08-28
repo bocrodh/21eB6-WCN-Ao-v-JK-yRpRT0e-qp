037c042a33dc7c597c4507ab4c328bb44a968d58
bocrodh <111758660+bocrodh@users.noreply.github.com>
2022-08-28 18:04:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ df_selection = df.query(
 ) 
 
 #st.dataframe (df_selection) 
-filtered = st.multiselect("Filter fields", options=df_selection.columns, default=df_selection["BB Number", "Relative Position of Canada","Links with domestic Payments Modernization work"])
+filtered = st.multiselect("Filter fields", options=df_selection.columns, default=df_selection[["BB Number", "Relative Position of Canada","Links with domestic Payments Modernization work"]])
 
 fig = go.Figure(data=[go.Table(
     header=dict(values=list(filtered),
