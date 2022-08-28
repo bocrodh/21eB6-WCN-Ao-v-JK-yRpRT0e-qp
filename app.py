a55fc07679ddd36e38b9042f86c5197aae6df584
bocrodh <111758660+bocrodh@users.noreply.github.com>
2022-08-28 18:23:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,4 @@ fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, height=800)
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.markdown('<style>body{background-color: #F6F5FO;}</style>',unsafe_allow_html=True)
-
 
