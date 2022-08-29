570b0a656018aeb25ca351f9f7c58bd113d6a4e1
bocrodh <111758660+bocrodh@users.noreply.github.com>
2022-08-28 20:32:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,14 +25,8 @@ df = df.drop_duplicates(keep='first')
 df = df[~df['Name of BB'].isnull()]   
 df = df.fillna('')
 
-#Hyperlinks
-planner_link = "https://tasks.office.com/BankofCanada.onmicrosoft.com/en-US/Home/Planner/#/plantaskboard?groupId=12185be2-5934-4383-a53c-1b41a10d5bc4&planId=4PAjzPLa2UChyp0AF-bvXn0AFp-h"
-st.sidebar.write("[Link to Planner](%s)" % planner_link) 
-sharepoint_link = "https://bankofcanada.sharepoint.com/teams/Cross_border_Payments/Shared%20Documents/Forms/AllItems.aspx" 
-st.sidebar.write("[Link to Sharepoint Page](%s)" % sharepoint_link)
-
 #Filters
-st.sidebar.header("Search Criteria:") 
+st.sidebar.header("Filter Criteria:") 
 
 bb_number = st.sidebar.multiselect( 
     "BB Number:",  
@@ -57,6 +51,13 @@ df_selection = df.query(
 ) 
 
 filtered = st.multiselect("Filter fields", options=df_selection.columns, default=['BB Number', 'Name of BB','Relative Position of Canada','Links with domestic Payments Modernization work'])
+
+#Hyperlinks
+st.sidebar.header("Useful Links:") 
+planner_link = "https://tasks.office.com/BankofCanada.onmicrosoft.com/en-US/Home/Planner/#/plantaskboard?groupId=12185be2-5934-4383-a53c-1b41a10d5bc4&planId=4PAjzPLa2UChyp0AF-bvXn0AFp-h"
+st.sidebar.write("[Link to Planner](%s)" % planner_link) 
+sharepoint_link = "https://bankofcanada.sharepoint.com/teams/Cross_border_Payments/Shared%20Documents/Forms/AllItems.aspx" 
+st.sidebar.write("[Link to Sharepoint Page](%s)" % sharepoint_link)
 
 #Plotly Dataframe
 fig = go.Figure(data=[go.Table(
